cb59880199117612b5b1e2966fbd9202b5b27d45
Jarno02 <jarno.ryhanen02@gmail.com>
2020-11-06 13:49:45 +0200

    added file reader

--- src/csvtranslator/CSVTranslator.java
+++ src/csvtranslator/CSVTranslator.java
@@ -1,21 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package csvtranslator;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 /**
- *
  * @author s1800870
  */
 public class CSVTranslator {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("args[0]: " + args[0] + ", args[1]: " + args[1]);    
+        csvReader();
+
     }
-    
+
+    private static void csvReader() {
+        String csvFile = "C:\\Users\\s1800885\\Documents\\CSVTranslator\\src\\csvtranslator\\CSVdata.csv";
+        BufferedReader br = null;
+        String line = "";
+        String splitter = ",";
+
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(splitter);
+
+         //       System.out.println("<String name=“”" + data[0] + ">" + data[2] + "</string>");
+                System.out.println(data[0] + ", " + data[1]+ ", "+ data[2] + ", " + data[3]);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
 }
