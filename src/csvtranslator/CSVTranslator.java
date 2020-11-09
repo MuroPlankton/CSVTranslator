7de59847138adaaae2f86415df894a4cc1ddb0fa
BC\s1800861 <richardo.nykanen@gmail.com>
2020-11-09 11:52:54 +0200

    Merge branch 'master' of https://github.com/MuroPlankton/CSVTranslator
    # Conflicts:
    #	.idea/misc.xml
    #	nbproject/private/private.xml
    #	out/production/CSVTranslator/csvtranslator/CSVTranslator.class
    #	out/production/CSVTranslator/csvtranslator/CSVdata.csv
    #	out/production/CSVTranslator/csvtranslator/CsvHandler.class
    #	strings.xml

--- src/csvtranslator/CSVTranslator.java
+++ src/csvtranslator/CSVTranslator.java
@@ -1,22 +1,6 @@
 package csvtranslator;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.text.Document;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author s1800870
