2f62d1902f000c8d9242a8f89ee46025bcfdcd51
Muro_Plankton <mirovaino@gmail.com>
2021-02-11 15:30:26 +0200

    Base UI for the new version
    Just the base UI form and class.

---
+++ src/csvtranslator/CsvAppUiClass.java
@@ -0,0 +1,22 @@
+package csvtranslator;
+
+import javax.swing.*;
+
+public class CsvAppUiClass {
+    private JPanel panel1;
+    private JList list1;
+    private JTextField libraryNameTextfield;
+    private JList textList;
+    private JPanel langAddingPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton button2;
+    private JTextField textNameField;
+    private JTextArea textDescArea;
+    private JTextField webKeyField;
+    private JTextField androidKeyField;
+    private JTextField iosKeyField;
+    private JSpinner langSpinner;
+    private JTextField translationField;
+}
