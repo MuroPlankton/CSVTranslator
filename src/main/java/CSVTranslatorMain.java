c3432f6a8fc59595b55c6922690e8616ff434f77
Muro_Plankton <mirovaino@gmail.com>
2021-02-15 14:48:08 +0200

    Updated gradle to latest version
    Still not perfect tho.

--- src/main/java/CSVTranslatorMain.java
+++ src/main/java/CSVTranslatorMain.java
@@ -1,16 +1,18 @@
+import auth.AuthHelper;
+
 import javax.swing.*;
 
 public class CSVTranslatorMain {
     public static void main(String[] args) {
         TranslatorUI translatorUI = new TranslatorUI();
         SwingUtilities.invokeLater(translatorUI.RunUI());
-        FireBaseRequests fireBaseRequests = new FireBaseRequests();
-        fireBaseRequests.put("https://csv-android-app-f0353-default-rtdb.firebaseio.com/user_libraries/feHvfGJ3Iwc8D565wQU7GHnH5hu2/testi/hauskaa.json", "{\n" +
-                "  \"testi\":\"tää tuli restistä\",\n" +
-                "  \"fjldskjfsdjgfjsd\": \"sgfsdkjfhkds\",\n" +
-                "  \"sfasdfdsfsdf\":\"sfdadsfsfds\"\n" +
-                "}");
+//        FireBaseRequests fireBaseRequests = new FireBaseRequests();
+//        fireBaseRequests.put("https://csv-android-app-f0353-default-rtdb.firebaseio.com/user_libraries/feHvfGJ3Iwc8D565wQU7GHnH5hu2/testi/hauskaa.json", "{\n" +
+//                "  \"testi\":\"tää tuli restistä\",\n" +
+//                "  \"fjldskjfsdjgfjsd\": \"sgfsdkjfhkds\",\n" +
+//                "  \"sfasdfdsfsdf\":\"sfdadsfsfds\"\n" +
+//                "}");
 
-
+        AuthHelper.signNewUserIn("mirovaino@gmail.com", "Muro_Plankton", "kauppakoti");
     }
 }
--- src/main/java/auth/AuthHelper.java
+++ src/main/java/auth/AuthHelper.java
@@ -1,14 +1,17 @@
 package auth;
 
-import okhttp3.OkHttp;
-import okhttp3.RequestBody;
+import okhttp3.*;
 
 public class AuthHelper {
-    public void signNewUserIn(String email, String displayName, String password) {
+    public static void signNewUserIn(String email, String displayName, String password) {
         String signInJson = String.format("{\"email\":\"%s\",\"password\":\"%s\",\"returnSecureToken\":true}", email, displayName);
 
-        OkHttp okHttp;
-        RequestBody body;
+        RequestBody body = RequestBody.create(signInJson, MediaType.parse("application/json"));
+
+        Request request = new Request.Builder()
+                .url("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBxx--8gER1rh7py1o6jYLWb_FiN5kvom4")
+                .post(body).build();
+
     }
 
     public void logExistingUserIn(String email, String password) {
