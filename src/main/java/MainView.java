4ce2e1fc4d1ce5a29f6490dde47e2bc6afb20252
Jarno02 <jarno.ryhanen02@gmail.com>
2021-02-19 14:48:08 +0200

    Added MainView

---
+++ src/main/java/MainView.java
@@ -0,0 +1,76 @@
+import javax.swing.*;
+import javax.swing.text.View;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainView {
+    private JPanel mainPanel;
+    private JPanel secondPanel;
+    private JSplitPane splitPane;
+    private JPanel mainView;
+    private JLabel librariesLabel;
+    private JList libraryList;
+    private JPanel thirdPane;
+    private JLabel libraryNameLabel;
+    private JList libraryAdapter;
+    private JPanel libraryActionsPane;
+    private JButton newTranslation;
+    private JTextField languageName;
+    private JTextField languageCode;
+    private JButton addButton;
+    private JButton importButton;
+    private JButton profileButton;
+    private JTextField translationNameTextField;
+    private JTextField translationDescrpitionTextField;
+    private JTextField androidKeyTextField;
+    private JTextField iosKeyTextField;
+    private JTextField webKeyTextField;
+    private JSpinner languageSpinner;
+    private JTextField translationNameTextField2;
+    private JButton saveButton;
+
+    private int buttonTracker;
+
+    public MainView() {
+//        ActionListener actionListener = new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//
+//            }
+//        };
+//
+//        addButton.addActionListener(actionListener);
+//        importButton.addActionListener(actionListener);
+//        profileButton.addActionListener(actionListener);
+        importButton.addActionListener(e -> {
+            System.out.println("import pressed");
+        });
+
+        addButton.addActionListener(e -> {
+            System.out.println("add pressed");
+        });
+
+        profileButton.addActionListener(e -> {
+            System.out.println("profile pressed");
+        });
+        newTranslation.addActionListener(e -> {
+            System.out.println("new translation pressed");
+        });
+
+        saveButton.addActionListener(e -> {
+            System.out.println("save pressed");
+        });
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Main view");
+        frame.setContentPane(new MainView().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+
+}
