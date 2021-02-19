f0c5bff3dd239141d1f81dd0f0a8e65ffe194ba6
MuroPlankton <73102721+MuroPlankton@users.noreply.github.com>
2021-02-19 12:52:14 +0200

    AuthHelper update 2
    It can now log an existing user in. It has a getter for the idToken. It has a setter for a user's displayName, which for now only sets it on firebase's authentication.

--- src/main/java/auth/AuthHelper.java
+++ src/main/java/auth/AuthHelper.java
@@ -17,6 +17,14 @@ public class AuthHelper {
 
     private static AuthHelper instance;
 
+    private String refreshToken;
+    private String idToken;
+    private long tokenExpiryTime;
+
+    private String userID;
+
+    private OkHttpClient client = new OkHttpClient();
+
     private AuthHelper() {
     }
 
@@ -28,9 +36,8 @@ public class AuthHelper {
     }
 
     public void signNewUserIn(String email, String displayName, String password) {
-        OkHttpClient client = new OkHttpClient();
-
-        String signInJson = String.format("{\"email\":\"%s\",\"password\":\"%s\",\"returnSecureToken\":true}", email, password);
+        String signInJson = String.format("{\"email\":\"%s\",\"password\":\"%s\",\"returnSecureToken\":true}",
+                email, password);
         RequestBody signInBody = RequestBody.create(signInJson, MediaType.parse("application/json"));
         Request signInRequest = new Request.Builder()
                 .url(String.format("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=%s", PROJECT_API_KEY))
@@ -39,45 +46,89 @@ public class AuthHelper {
         try {
             JsonElement signInElement = JsonParser.parseString(client.newCall(signInRequest).execute().body().string());
             signInObject = signInElement.getAsJsonObject();
-            System.out.println(signInElement);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        String idToken = signInObject.get("idToken").getAsString();
-        System.out.println(idToken);
-        String refreshToken = signInObject.get("refreshToken").getAsString();
-        String Uid = signInObject.get("localId").getAsString();
+        tokenExpiryTime = System.currentTimeMillis() + 3600;
+        idToken = signInObject.get("idToken").getAsString();
+        refreshToken = signInObject.get("refreshToken").getAsString();
+        userID = signInObject.get("localId").getAsString();
 
-        String setNameJson = String.format("{\n" +
-                " \"idToken\":\"%s\",\n" +
-                " \"displayName\":\"%s\",\n" +
-                " \"returnSecureToken\":false\n" +
-                "}", idToken, displayName);
-        System.out.println(setNameJson);
-        RequestBody setNameBody = RequestBody.create(setNameJson, MediaType.parse("application/json"));
-        Request setNameRequest = new Request.Builder()
-                .url(String.format("https://identitytoolkit.googleapis.com/v1/accounts:update?key=%s", PROJECT_API_KEY))
-                .post(setNameBody).build();
-        try {
-            System.out.println(client.newCall(setNameRequest).execute().body().string());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        setNewDisplayName(displayName);
 
-        String addUserToRealtimeJson = String.format("{\"%s\":\"%s\"}", Uid, displayName);
+        String addUserToRealtimeJson = String.format("{\"%s\":\"%s\"}", userID, displayName);
         RequestBody addRealtimeUserRB = RequestBody.create(addUserToRealtimeJson, MediaType.parse("application/json"));
         Request addRealtimeUserRequest = new Request.Builder()
                 .url(String.format("https://%s.firebaseio.com/users.json?auth=%s", PROJECT_ID, idToken))
                 .patch(addRealtimeUserRB).build();
         try {
-            System.out.println(client.newCall(addRealtimeUserRequest).execute().body().string());
+            client.newCall(addRealtimeUserRequest).execute().body().string();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void logExistingUserIn(String email, String password) {
+        String signInJson = String.format("{\"email\":\"%s\",\"password\":\"%s\",\"returnSecureToken\":true}",
+                email, password);
+        RequestBody signInBody = RequestBody.create(signInJson, MediaType.parse("application/json"));
+        Request signInRequest = new Request.Builder()
+                .url(String.format("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=%s", PROJECT_API_KEY))
+                .post(signInBody).build();
+        JsonObject signInObject = null;
+        try {
+            JsonElement signInElement = JsonParser.parseString(client.newCall(signInRequest).execute().body().string());
+            signInObject = signInElement.getAsJsonObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        tokenExpiryTime = System.currentTimeMillis() + 3600;
+        idToken = signInObject.get("idToken").getAsString();
+        refreshToken = signInObject.get("refreshToken").getAsString();
+        userID = signInObject.get("localId").getAsString();
+    }
+
+    public String getIDToken() {
+        if (tokenExpiryTime < System.currentTimeMillis()) {
+            return idToken;
+        } else {
+            String tokenRefreshRBString = String.format("grant_type=refresh_token&refresh_token=%s", refreshToken);
+            RequestBody tokenRefreshRB = RequestBody.create(tokenRefreshRBString,
+                    MediaType.parse("application/x-www-form-urlencoded"));
+            Request tokenRefreshRequest = new Request.Builder()
+                    .url(String.format("https://securetoken.googleapis.com/v1/token?key=%s", PROJECT_API_KEY))
+                    .post(tokenRefreshRB).build();
+            try {
+                String responseBodyString = JsonParser.parseString(
+                        client.newCall(tokenRefreshRequest).execute().body().string())
+                        .getAsJsonObject().get("id_token").getAsString();
+                return responseBodyString;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    public String getDisplayName() {
+        return null;
+    }
+
+    public void setNewDisplayName(String displayName) {
+        String setNameJson = String.format("{\"idToken\":\"%s\",\"displayName\":\"%s\",\"returnSecureToken\":false}",
+                getIDToken(), displayName);
+        System.out.println(setNameJson);
+        RequestBody setNameBody = RequestBody.create(setNameJson, MediaType.parse("application/json"));
+        Request setNameRequest = new Request.Builder()
+                .url(String.format("https://identitytoolkit.googleapis.com/v1/accounts:update?key=%s", PROJECT_API_KEY))
+                .post(setNameBody).build();
+        try {
+            client.newCall(setNameRequest).execute().body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
