ae17a83fc01699090a3d10215d03acb8c8dcc41e
Jarno02 <jarno.ryhanen02@gmail.com>
2021-03-01 14:10:25 +0200

    Added methods that update libraries in Firebase

--- src/main/java/MainView.java
+++ src/main/java/MainView.java
@@ -1,6 +1,10 @@
+import com.intellij.uiDesigner.compiler.StringPropertyCodeGenerator;
+
 import javax.swing.*;
 
 public class MainView {
+    FireBaseRequests fireBaseRequests = new FireBaseRequests();
+
     private JPanel mainPanel;
     private JPanel secondPanel;
     private JSplitPane splitPane;
@@ -12,8 +16,8 @@ public class MainView {
     private JList libraryAdapter;
     private JPanel libraryActionsPane;
     private JButton newTranslation;
-    private JTextField languageName;
-    private JTextField languageCode;
+    private JTextField languageNameTextField;
+    private JTextField languageCodeTextField;
     private JButton addButton;
     private JTextField translationNameTextField;
     private JTextField translationDescrpitionTextField;
@@ -35,35 +39,77 @@ public class MainView {
     private JComboBox languagesDropDown;
 
     private int languageCount;
+    private final String userID = "";
+    private final String userIDToken = "";
+    private final String libraryID = "";
 
     public MainView() {
 
-        addButton.addActionListener(e -> {
-            System.out.println("add pressed");
-        });
-
-        newTranslation.addActionListener(e -> {
-            System.out.println("new translation pressed");
-        });
+        addButton.addActionListener(e -> addNewLanguage());
+        newTranslation.addActionListener(e -> System.out.println("new translation pressed"));
 
         saveButton.addActionListener(e -> {
-            System.out.println("save pressed");
+            addTranslationToLibraries();
         });
+
         addLanguageToDropDown("suomi");
         addLanguageToDropDown("english");
         addLanguageToDropDown("svenska");
 
-        languagesDropDown.addActionListener(e -> System.out.println(languagesDropDown.getItemAt(languagesDropDown.getSelectedIndex()).toString()));
-
         languageCountLabe.setText(String.format("amount of languages: $d", languageCount));
+    }
+
+    private void addNewLanguage() {
+        String languageCode = languageCodeTextField.getText();
+        String languageName = languageNameTextField.getText();
+
+        String jsonBody = "{\n" +
+                "  \" " + languageCode + "\":\"" + languageName + "\"\n" +
+                "}";
+
+        String url = "https://csv-android-app-f0353-default-rtdb.firebaseio.com/libraries/" + libraryID + "/languages.json?auth=" + userIDToken;
+
+        fireBaseRequests.addData(url, jsonBody);
+    }
+
+    private void addTranslationToLibraries() {
+        String translationName = translationDescrpitionTextField.getText();
+        String translationDescription = translationDescrpitionTextField.getText();
+        String androidKey = androidKeyTextField.getText();
+        String iosKey = iosKeyTextField.getText();
+        String webKey = webKeyTextField.getText();
+        String language = languagesDropDown.getItemAt(languagesDropDown.getSelectedIndex()).toString();
+        String translation = translationNameTextField2.getText();
+        System.out.println("save pressed");
+        String translationID = "";
+
+        String jsonBody = "{\n" +
+                "  \"android_key\":\" " + androidKey + "\",\n" +
+                "  \"ios_key\":\" " + iosKey + "\",\n" +
+                "  \"web_key\":\" " + webKey + "\",\n" +
+                "  \"name\":\" " + translationName + "\",\n" +
+                "  \"description\":\" " + translationDescription + "\"\n" +
+                "  \"translations\":{\n" +
+                "  \" " + language + "\":\" " + translation + "\",\n" +
+                "\t}\n" +
+                "}\n";
+
+        String url = "https://csv-android-app-f0353-default-rtdb.firebaseio.com/libraries/" + libraryID + "/texts/" + translationID + ".json?auth=" + userIDToken;
+
+        fireBaseRequests.addData(url, jsonBody);
     }
 
     private static void makeJMenu(JFrame frame) {
         JMenu jMenu = new JMenu("Options");
+        JMenuBar jMenuBar = new JMenuBar();
+
         JMenuItem importFile = new JMenuItem("Import");
         JMenuItem profile = new JMenuItem("Profile");
         JMenuItem addNewFile = new JMenuItem("Add new file");
-        JMenuBar jMenuBar = new JMenuBar();
+
+        importFile.addActionListener(e -> System.out.println("import pressed"));
+        profile.addActionListener(e -> System.out.println("profile pressed"));
+        addNewFile.addActionListener(e -> System.out.println("add new text pressed"));
 
         jMenu.add(addNewFile);
         jMenu.add(profile);
@@ -85,6 +131,4 @@ public class MainView {
         frame.setResizable(false);
         frame.setVisible(true);
     }
-
-
 }
