c3d6f0b701cb97cbe4f86060a9638f93b5b34c8d
Jarno02 <jarno.ryhanen02@gmail.com>
2021-03-01 14:30:52 +0200

    minor changes

--- src/main/java/MainView.java
+++ src/main/java/MainView.java
@@ -40,8 +40,8 @@ public class MainView {
 
     private int languageCount;
     private final String userID = "";
-    private final String userIDToken = "";
-    private final String libraryID = "";
+    private final String userIDToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImM0ZWFhZjkxM2VlNWY0MDY0YmE2NjUzN2M0Njk3YzY5OGE3NGYwODIiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiamFybm8iLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vY3N2LWFuZHJvaWQtYXBwLWYwMzUzIiwiYXVkIjoiY3N2LWFuZHJvaWQtYXBwLWYwMzUzIiwiYXV0aF90aW1lIjoxNjE0NjAwNTA0LCJ1c2VyX2lkIjoiZmVIdmZHSjNJd2M4RDU2NXdRVTdHSG5INWh1MiIsInN1YiI6ImZlSHZmR0ozSXdjOEQ1NjV3UVU3R0huSDVodTIiLCJpYXQiOjE2MTQ2MDA1MDQsImV4cCI6MTYxNDYwNDEwNCwiZW1haWwiOiJyeWhhbmVuamFybm9AZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbInJ5aGFuZW5qYXJub0BnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.RYKe5k8Gn4PB3ZIn45RdeTtnE0rZ6tWv1Rl6Ytu23AYf0qfSyypNXsb-6h1U_oHK_EnY90VU6hEik1gv6hQ7pWq786ruMTlcIdXXOxJTNl88X5iT4pZtUzoFXSoKfy6MI6QWM2bSBzjzoNd_hGCgNNvSd7tO4F36mzEmpYJoQlaNkewL2mVKu2_Fx8uGo4Iy3qYKG8eIWocVlW-MEVKzUR9rSillvIdSwwI4KeCrthNXiN9EcaTE5Dwpb7slHuR_NaFAYuA7IQ7BXPvZruyLihTFDakUlhAxdL18ph-hX7aYvxlsVTzpebk65s81OSTfQS7x6732SRgH4OwM0WM4IA";
+    private final String libraryID = "044fe665-821d-4e7e-be96-678125a40527";
 
     public MainView() {
 
@@ -81,21 +81,22 @@ public class MainView {
         String language = languagesDropDown.getItemAt(languagesDropDown.getSelectedIndex()).toString();
         String translation = translationNameTextField2.getText();
         System.out.println("save pressed");
-        String translationID = "";
+        String translationID = "abc";
 
         String jsonBody = "{\n" +
-                "  \"android_key\":\" " + androidKey + "\",\n" +
-                "  \"ios_key\":\" " + iosKey + "\",\n" +
-                "  \"web_key\":\" " + webKey + "\",\n" +
-                "  \"name\":\" " + translationName + "\",\n" +
-                "  \"description\":\" " + translationDescription + "\"\n" +
+                "  \"android_key\":\"" + androidKey + "\",\n" +
+                "  \"ios_key\":\"" + iosKey + "\",\n" +
+                "  \"web_key\":\"" + webKey + "\",\n" +
+                "  \"name\":\"" + translationName + "\",\n" +
+                "  \"description\":\"" + translationDescription + "\",\n" +
                 "  \"translations\":{\n" +
-                "  \" " + language + "\":\" " + translation + "\",\n" +
+                "  \"" + language + "\":\"" + translation + "\"\n" +
                 "\t}\n" +
                 "}\n";
 
         String url = "https://csv-android-app-f0353-default-rtdb.firebaseio.com/libraries/" + libraryID + "/texts/" + translationID + ".json?auth=" + userIDToken;
-
+        System.out.println(jsonBody);
+        System.out.println(url);
         fireBaseRequests.addData(url, jsonBody);
     }
 
